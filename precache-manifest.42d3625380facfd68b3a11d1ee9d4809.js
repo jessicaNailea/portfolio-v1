fe656c4eb2150bb182da6db08031d6de2c55551a
jessicaBuenfil <jessibuenfilsolis@gmail.com>
2020-05-10 17:23:42 -0500

    Updates

---
+++ precache-manifest.42d3625380facfd68b3a11d1ee9d4809.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b63f525c5265f03495cdb65e6545e5",
+    "url": "/portfolio-v1/index.html"
+  },
+  {
+    "revision": "44dae6c4bd1a138bca08",
+    "url": "/portfolio-v1/static/css/2.dfcd306b.chunk.css"
+  },
+  {
+    "revision": "3b769e1e06e771f115dd",
+    "url": "/portfolio-v1/static/css/main.0067ea5a.chunk.css"
+  },
+  {
+    "revision": "44dae6c4bd1a138bca08",
+    "url": "/portfolio-v1/static/js/2.c2d07d2e.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/portfolio-v1/static/js/2.c2d07d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b769e1e06e771f115dd",
+    "url": "/portfolio-v1/static/js/main.1d3c15b8.chunk.js"
+  },
+  {
+    "revision": "5bbde024aed08de7217b",
+    "url": "/portfolio-v1/static/js/runtime-main.f9a040b0.js"
+  },
+  {
+    "revision": "c18ca5c5ed76df84bcade87c5f50d90b",
+    "url": "/portfolio-v1/static/media/coati.c18ca5c5.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio-v1/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio-v1/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio-v1/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio-v1/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio-v1/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e6da8edf3adaad03c82d9784453c7832",
+    "url": "/portfolio-v1/static/media/profile.e6da8edf.png"
+  }
+]);
